ef3a9e245527b9e4306c351acc42e8e85bb8a111
Yannick <20306025+yandeu@users.noreply.github.com>
2020-05-22 17:53:20 +0200

    .

--- packages/dev/src/game.ts
+++ packages/dev/src/game.ts
@@ -2,10 +2,12 @@ import startPhaserGame from './phaserGame'
 import startProject from './threeScene'
 import startPhysics from './physics'
 import startHeadless from './headless'
+import startBabylon from './babylon'
 
 // uncomment the one you want to debug
 
 // startPhaserGame() // @enable3d/phaser-extension
-startProject() // @enable3d/three-graphics
+// startProject() // @enable3d/three-graphics
 // startPhysics() // @enable3d/ammo-physics
 // startHeadless() // @enable3d/ammo-physics (headless)
+startBabylon() // babylon physics experiment
